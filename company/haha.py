7b10c9cab868ad36b89e007accdb871ad3403e6b
kimbonsu004 <bonsu04@naver.com>
2021-10-04 18:17:11 +0900

    gitignore , user.py added

--- company/haha.py
+++ company/haha.py
@@ -3,18 +3,22 @@ from time import *
 
 def price():
 
-    haha_price=randint(1000,10000) # 주식 현재가 , 1000원에서 10000원 사이 가격
+    abc_price=randint(1000,10000) # 주식 현재가 , 1000원에서 10000원 사이 가격
 
-    change=haha_price/randint(50,500) # 증감량 , 10& 이내에서 등락
+    while True:
 
-    per=randint(1,100) # 증감 확률
+        change=round(abc_price/randint(50,500)) # 증감량 , 10& 이내에서 등락
 
-    if per>=40:      # 60프로 확률
-        haha_price+=change
-    else:
-        haha_price-=change
-    
-    return haha_price
+        per=randint(1,100) # 증감 확률
+
+        if per>=40:      # 60프로 확률
+            abc_price+=change
+        else:
+            abc_price-=change
+        
+        print("\r 현재 가격 : {}".format(abc_price),end="")
+
+        sleep(10)
 
 
 
